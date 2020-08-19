eca2e6973a467622bdc6e98ba15b9b4e24c2e685
Rhys Hoad <rhyshoad@gmail.com>
2020-08-19 11:50:40 +0100

    uncommented

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "example-terraform-project-name-bootstrap-terraform-state"
-#     key    = "default-infrastructure"
-#     region = "eu-central-1"
-#   }
-# }
+ terraform {
+   backend "s3" {
+     bucket = "example-terraform-project-name-bootstrap-terraform-state"
+     key    = "default-infrastructure"
+     region = "eu-central-1"
+   }
+ }
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "example-terraform-project-name-bootstrap-terraform-state"
