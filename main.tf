bcba0ae9f6bc89c7090e2675677b5f743f0d9129
Rhys Hoad <rhyshoad@gmail.com>
2020-08-19 11:56:21 +0100

    update

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
+#set provider and region
 provider "aws" {
   version = "~> 2.0"
   region  = "eu-central-1"
 }
-
+#define terraform backend (S3) and bucket name & region
  terraform {
    backend "s3" {
      bucket = "example-terraform-project-name-bootstrap-terraform-state"
@@ -10,10 +11,10 @@ provider "aws" {
      region = "eu-central-1"
    }
  }
-
+#define the resource within the bucket
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "example-terraform-project-name-bootstrap-terraform-state"
-
+#ensure versioning is enabled for robustness and RPO
   versioning {
     enabled = true
   }
